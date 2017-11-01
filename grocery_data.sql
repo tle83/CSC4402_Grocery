08be680c94da92e053dda64b230983651574c8b9
Tuyet Le <tnle833@gmail.com>
2017-11-01 12:32:43 -0500

    initial upload
    -add table creation
    -add test data

---
+++ grocery_data.sql
@@ -0,0 +1,15 @@
+/* Clear all tables before inserting */
+delete from Item;
+delete from Vendor;
+delete from Rep;
+delete from Driver;
+delete from Checkout;
+delete from Employee;
+
+/* Insert into item tables */
+insert into Item values ('001', 'Apples',1.50);
+/* Insert into vendor tables */
+/* Insert into representation tables */
+/* Insert into driver tables */
+/* Insert into checkout tables */
+/* Insert into employee tables */
---
+++ grocery_tables.sql
@@ -0,0 +1,26 @@
+Create table Item(
+    I_ID varchar(20),
+    I_name  varchar(20),
+    price   integer,
+    primary key (I_ID)
+);
+
+Create table Vendor(
+
+);
+
+Create table Rep(
+
+);
+
+Create table Driver(
+
+);
+
+Create table Checkout(
+
+);
+
+Create table Employee(
+
+);
