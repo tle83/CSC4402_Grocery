879bb9582e215cb6f9b34ea53855bcbb7d9bb2e7
Julian Dane Plaisance <jplais8@lsu.edu>
2017-11-01 14:16:18 -0500

    filled the other tables based on google Doc
    Probably will be changed later, but its a starting point for us

--- grocery_tables.sql
+++ grocery_tables.sql
@@ -1,26 +1,52 @@
 Create table Item(
-    I_ID varchar(20),
-    I_name  varchar(20),
-    price   integer,
-    primary key (I_ID)
+	I_ID varchar(20),
+	I_name  varchar(20),
+	price   integer,
+	primary key (I_ID)
 );
 
 Create table Vendor(
-
+	I_ID varchar(20),
+	V_ID varchar(20),
+	V_name varchar(20),
+	Price integer,--probably not needed
+	V_PNum varchar(15),
+	foreign key(I_ID) references Item,
+	primary key(V_ID)
 );
 
 Create table Rep(
-
+	V_ID varchar(20),
+	R_ID varchar(20),
+	R_Name varchar(20),
+	R_PNum varchar(15),
+	Hours_of_Op varchar(40),
+	foreign key(V_ID) references Vendor,
+	primary key(R_ID)
 );
 
 Create table Driver(
-
+	V_ID varchar(20),
+	D_ID varchar(20),
+	D_Name varchar(20),
+	D_PNum varchar(15),
+	Hours_of_Deliv varchar(40),
+	foreign key(V_ID) references Vendor,
+	primary key(D_ID)
 );
 
 Create table Checkout(
-
+	Receipt_ID varchar(20),
+	E_ID varchar(20),
+	Num_Items integer,
+	Ttl_Price integer,
+	foreign key(E_ID) references Employee,
+	primary key(Receipt_ID)
 );
 
 Create table Employee(
-
+	E_ID varchar(20),
+	E_Name varchar(20),
+	Hr_Wage float,
+	primary key(E_ID)
 );
