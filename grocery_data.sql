f7e5e610671dde09b7391a09851a99300d7c821a
Tuyet Le <tnle833@gmail.com>
2017-11-14 11:28:13 -0600

    Add basic Vendor data

--- grocery_data.sql
+++ grocery_data.sql
@@ -11,34 +11,61 @@ delete from Employee;
 insert into Store values('S1', '4402_Grocery', '385 Nicholson Dr', 'Baton Rouge', 'Louisiana');
 
 /* Insert into item tables */
-insert into Item values ('001', 'Apples',1.50);
-insert into Item values ('002', 'Grapes', 3.00);
-insert into Item values ('003', 'Milk', 5.75);
-insert into Item values ('004', 'Eggs', 3.25);
-insert into Item values ('005', 'Yogurt', 3.00);
-insert into Item values ('006', 'Orange Juice', 2.50);
-insert into Item values ('007', 'Coke', 1.25);
-insert into Item values ('008', 'Sprite', 1.25);
-insert into Item values ('009', 'Doritos', 1.00);
-insert into Item values ('010', 'Cheetos', 1.00);
-insert into Item values ('011', 'Hersheys', 1.00);
-insert into Item values ('012', 'Onions', 1.75);
-insert into Item values ('013', 'Garlic', 1.50);
-insert into Item values ('014', 'Lettuce', 1.25);
-insert into Item values ('015', 'Orange', 2.25);
-insert into Item values ('016', 'Cherries', 2.25);
-insert into Item values ('017', 'Pineapple', 3.25);
-insert into Item values ('018', 'Cereal', 2.00);
-insert into Item values ('019', 'Flour', 1.75);
-insert into Item values ('020', 'Bread', 1.75);
-insert into Item values ('021', 'Ice Cream', 1.50);
-insert into Item values ('022', 'Pizza', 5.00);
-insert into Item values ('023', 'Cookies', 2.50);
-insert into Item values ('024', 'Cooking oil', 3.00);
-insert into Item values ('025', 'Salt', 1.25);
-insert into Item values ('026', 'Pepper', 1.25);
+insert into Item values ('I001', 'Apples',1.50);
+insert into Item values ('I002', 'Grapes', 3.00);
+insert into Item values ('I003', 'Milk', 5.75);
+insert into Item values ('I004', 'Eggs', 3.25);
+insert into Item values ('I005', 'Yogurt', 3.00);
+insert into Item values ('I006', 'Orange Juice', 2.50);
+insert into Item values ('I007', 'Coke', 1.25);
+insert into Item values ('I008', 'Sprite', 1.25);
+insert into Item values ('I009', 'Doritos', 1.00);
+insert into Item values ('I010', 'Cheetos', 1.00);
+insert into Item values ('I011', 'Hersheys', 1.00);
+insert into Item values ('I012', 'Onions', 1.75);
+insert into Item values ('I013', 'Garlic', 1.50);
+insert into Item values ('I014', 'Lettuce', 1.25);
+insert into Item values ('I015', 'Orange', 2.25);
+insert into Item values ('I016', 'Cherries', 2.25);
+insert into Item values ('I017', 'Pineapple', 3.25);
+insert into Item values ('I018', 'Cereal', 2.00);
+insert into Item values ('I019', 'Flour', 1.75);
+insert into Item values ('I020', 'Bread', 1.75);
+insert into Item values ('I021', 'Ice Cream', 1.50);
+insert into Item values ('I022', 'Pizza', 5.00);
+insert into Item values ('I023', 'Cookies', 2.50);
+insert into Item values ('I024', 'Cooking oil', 3.00);
+insert into Item values ('I025', 'Salt', 1.25);
+insert into Item values ('I026', 'Pepper', 1.25);
 
 /* Insert into vendor tables */
+insert into Vendor values ('I001', 'V001', 'Farmers Market', '2252894732');
+insert into Vendor values ('I002', 'V002', 'Farmers Market', '2252894732');
+insert into Vendor values ('I003', 'V003', 'Tigers Produce', '2254896825');
+insert into Vendor values ('I004', 'V003', 'Tigers Produce', '2254896825');
+insert into Vendor values ('I005', 'V003', 'Tigers Produce', '2254896825');
+insert into Vendor values ('I006', 'V003', 'Tigers Produce', '2254896825');
+insert into Vendor values ('I007', 'V004', 'Mikes Beverages', '2252537943');
+insert into Vendor values ('I008', 'V004', 'Mikes Beverages', '2252537943');
+insert into Vendor values ('I009', 'V005', 'Tiger Snacks', '2253109438');
+insert into Vendor values ('I010', 'V005','Tiger Snacks', '2253109438');
+insert into Vendor values ('I011', 'V005','Tiger Snacks', '2253109438');
+insert into Vendor values ('I012', 'V006', 'Southern Fresh Veggies', '2250784321');
+insert into Vendor values ('I013', 'V006', 'Southern Fresh Veggies', '2250784321');
+insert into Vendor values ('I014', 'V006', 'Southern Fresh Veggies', '2250784321');
+insert into Vendor values ('I015', 'V001', 'Farmers Market', '2252894732');
+insert into Vendor values ('I016', 'V001', 'Farmers Market', '2252894732');
+insert into Vendor values ('I017', 'V001', 'Farmers Market', '2252894732');
+insert into Vendor values ('I018', 'V007', 'LA Grains', '2257328465');
+insert into Vendor values ('I019', 'V008', 'Souths Baking Goods', '2253125343');
+insert into Vendor values ('I020', 'V009', 'LA Bakery', '2252347654');
+insert into Vendor values ('I021', 'V003', 'Tigers Produce', '2254896825');
+insert into Vendor values ('I022', 'V010', 'Tiger Pizza', '2251234567');
+insert into Vendor values ('I023', 'V009', 'LA Bakery', '2252347654');
+insert into Vendor values ('I024', 'V008', 'Souths Baking Goods', '2253125343');
+insert into Vendor values ('I025', 'V011', 'Salt Life', '2253217383');
+insert into Vendor values ('I026', 'V012', 'LA Spices', '2250349812');
+
 /* Insert into representation tables */
 /* Insert into driver tables */
 /* Insert into checkout tables */
