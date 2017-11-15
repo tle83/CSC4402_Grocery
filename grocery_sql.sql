89b55868392709b144c88ec4459d421305b80200
Thanh Trinh <taniatrinh@tanias-mbp-2.attlocal.net>
2017-11-15 14:12:15 -0600

    update sql query in grocery_sql.sql

--- grocery_sql.sql
+++ grocery_sql.sql
@@ -3,6 +3,16 @@
 /* 1) Get vendor ID such that it provides the grocery store with more than 4 different items */
 
 
-/* 2) Get item ID such that the store */
+/* 2) Get item ID such that the store receives the item by more than 2 vendors */
+
 
 /* 3) */
+/* 4) */
+/* 5) */
+/* 6) */
+/* 7) */
+/* 8) */
+/* 9) */
+/* 10) */
+/* 11) */
+
