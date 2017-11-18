41748ba0c9ea2669b8ba1f2d4ac094a3fa151057
alliza <bistaaliza@gmail.com>
2017-11-17 22:31:15 -0600

    Update grocery_sql.sql

--- grocery_sql.sql
+++ grocery_sql.sql
@@ -1,9 +1,14 @@
 /* Grocery select and update SQL statements */
 
 /* 1) Get vendor ID such that it provides the grocery store with more than 4 different items */
+      select V_ID 
+      from (select count(I_ID) as num, V_ID from inventory group by V_ID) as a 
+      where num > 4;
 
-
-/* 2) Get item ID such that the store receives the item by more than 2 vendors */
+/* 2) Get item ID such that the store receives the item by more than 2 vendors */ 
+      select I_ID 
+      from (select I_ID, count(V_ID) as num from inventory group by V_ID) as b
+      where num > 2;
 
 
 /* 3) */
