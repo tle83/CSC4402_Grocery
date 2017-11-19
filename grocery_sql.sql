0db902d99d578239894777976ec8da11f832d81b
Thanh Trinh <trinhtania@gmail.com>
2017-11-19 14:37:13 -0600

    update grocery_sql.sql

--- grocery_sql.sql
+++ grocery_sql.sql
@@ -10,7 +10,6 @@
       from (select I_ID, count(V_ID) as num from inventory group by V_ID) as b
       where num > 2;
 
-
 /* 3) Get I_ID and Item name with Price greater than 1 and receive by Vendor Located in Baton Rouge. */
       Select I_ID, I_name 
       from inventory join item using(I_ID) join vendor using(V_ID)
@@ -20,10 +19,12 @@
       select D_ID, D_Name, D_PNum 
       from driver join vonder using(V_ID)
       where V_city = 'New Orleans';
+
 /* 5) List  Id , Name and Phone number of all the representive who does not work in Metairie.*/
       Select R_ID, R_Name, R_PNum
       from vendor join rep using(V_ID)
       where V_City <> 'Metairie';
+
 /* 6) Get vendor ID and name which is located in Baton Rouge with more than 4 different items*/
       select distinct(vendor.V_ID), V_Name 
       from inventory left outer join vendor on vendor.V_ID = inventory.V_ID 
@@ -31,14 +32,17 @@
       and vendor.V_ID in (select V_ID 
                           from (select count(I_ID) as num, V_ID from inventory group by V_ID) as a 
                           where num > 4);
+
 /* 7) List all the vendors Id, name and phone number located in Baton Rouge*/
       Select V_ID, V_Name, V_PNum
       from vendor
       where V_city = 'Baton Rouge';
+
 /* 8) List all vendors ID where more than one Representive */
       Select V_ID 
       from (Select V_ID, count(R_ID) as num from rep group by (V_ID)) as a
       where num > 1;
+      
 /* 9)  List all the R_ID, R_Name, R_PNum who works in Vendor ID V009*/
       Select R_ID, R-Name, R_PNum
       from rep 
@@ -48,6 +52,7 @@
       Select I_name
       from item
       where Price > 2;
+      
 /* 11) List all the drives ID, Name and PNumber who works in vendor located in Baton Rouge*/
       Select D_ID, D_Name, D_PNum
       from driver join vendor using(V_ID) 
