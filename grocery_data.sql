b1658c4439979722e7d49c6b845de8bca7898e6c
Thanh Trinh <trinhtania@gmail.com>
2017-11-24 17:27:12 -0600

    update values in grocery_data.sql

--- grocery_data.sql
+++ grocery_data.sql
@@ -38,52 +38,39 @@ insert into Item values ('I026', 'Pepper', 1.25);
 /* Insert into inventory table */
 insert into Inventory values ('I001', 'V001');
 insert into Inventory values ('I001', 'V002');
-insert into Inventory values ('I001', 'V006');
 insert into Inventory values ('I002', 'V001');
-insert into Inventory values ('I002', 'V002');
-insert into Inventory values ('I002', 'V006');
-insert into Inventory values ('I003', 'V002');
+insert into Inventory values ('I002', 'V020');
+insert into Inventory values ('I003', 'V004');
+insert into Inventory values ('I004', 'V023');
 insert into Inventory values ('I005', 'V005');
 insert into Inventory values ('I006', 'V004');
+insert into Inventory values ('I007', 'V004');
+insert into Inventory values ('I008', 'V013');
 insert into Inventory values ('I009', 'V005');
 insert into Inventory values ('I010', 'V005');
-insert into Inventory values ('I012', 'V001');
+insert into Inventory values ('I011', 'V010');
 insert into Inventory values ('I012', 'V006');
-insert into Inventory values ('I014', 'V002');
-insert into Inventory values ('I015', 'V001');
-insert into Inventory values ('I015', 'V006');
-insert into Inventory values ('I018', 'V002');
-insert into Inventory values ('I018', 'V005');
-insert into Inventory values ('I019', 'V007');
+insert into Inventory values ('I013', 'V018');
+insert into Inventory values ('I014', 'V014');
+insert into Inventory values ('I015', 'V004');
+insert into Inventory values ('I016', 'V002');
+insert into Inventory values ('I017', 'V005');
+insert into Inventory values ('I018', 'V017');
 insert into Inventory values ('I019', 'V008');
-insert into Inventory values ('I021', 'V002');
-insert into Inventory values ('I021', 'V005');
+insert into Inventory values ('I020', 'V002');
+insert into Inventory values ('I021', 'V021');
 insert into Inventory values ('I022', 'V009');
-insert into Inventory values ('I024', 'V003');
-insert into Inventory values ('I001', 'V019');
+insert into Inventory values ('I024', 'V007');
+insert into Inventory values ('I023', 'V019');
 insert into Inventory values ('I001', 'V024');
 insert into Inventory values ('I002', 'V025');
-insert into Inventory values ('I002', 'V021');
+insert into Inventory values ('I025', 'V003');
 insert into Inventory values ('I003', 'V022');
-insert into Inventory values ('I005', 'V018');
-insert into Inventory values ('I005', 'V017');
+insert into Inventory values ('I026', 'V012');
+insert into Inventory values ('I019', 'V015');
 insert into Inventory values ('I008', 'V016');
 insert into Inventory values ('I009', 'V011');
-insert into Inventory values ('I011', 'V010');
-insert into Inventory values ('I012', 'V012');
-insert into Inventory values ('I013', 'V013');
-insert into Inventory values ('I014', 'V017');
-insert into Inventory values ('I014', 'V016');
-insert into Inventory values ('I015', 'V015');
-insert into Inventory values ('I015', 'V013');
-insert into Inventory values ('I016', 'V014');
-insert into Inventory values ('I018', 'V011');
-insert into Inventory values ('I018', 'V020');
-insert into Inventory values ('I019', 'V021');
-insert into Inventory values ('I019', 'V022');
-insert into Inventory values ('I020', 'V023');
-insert into Inventory values ('I021', 'V024');
-insert into Inventory values ('I021', 'V025');
+
 
 /* Insert into vendor table */
 insert into Vendor values ('V001', 'Farmers Market', '2252894732', 'Baton Rouge');
@@ -98,18 +85,18 @@ insert into Vendor values ('V009', 'Tiger Pizza', '2251234567', 'Baton Rouge');
 insert into Vendor values ('V010', 'American Market', '2259282232', 'Baton Rouge');
 insert into Vendor values ('V011', 'Lions Produce', '2254090805', 'Lafayette');
 insert into Vendor values ('V012', 'Cajun Spices', '5041111111', 'New Orleans');
-insert into Vendor values ('V013', 'Mikes drinks', '2252532223', 'Baton Rouge');
-insert into Vendor values ('V014', 'Met air veggies', '2253125343', 'Metairie');
+insert into Vendor values ('V013', 'Mikes Drinks', '2252532223', 'Baton Rouge');
+insert into Vendor values ('V014', 'Met Air Veggies', '2253125343', 'Metairie');
 insert into Vendor values ('V015', 'Tiger Tacos', '2251234568', 'Baton Rouge');
 insert into Vendor values ('V016', 'Market for DBMAs', '2250009341', 'Baton Rouge');
 insert into Vendor values ('V017', 'Produce for Tigres', '2254292929', 'Lafayette');
 insert into Vendor values ('V018', 'Cajun & Chinese food', '2250341112', 'Metairie');
 insert into Vendor values ('V019', 'Basta Pasta', '2250000000', 'Baton Rouge');
 insert into Vendor values ('V020', 'Store by Gore', '2250000001', 'New Orleans');
-insert into Vendor values ('V021', 'Griffiths food emporium', '2250000002', 'New Orleans');
+insert into Vendor values ('V021', 'Griffiths Food Emporium', '2250000002', 'New Orleans');
 insert into Vendor values ('V022', 'Le Home Resturant', '2250000003', 'Metairie');
 insert into Vendor values ('V023', 'Nguyen New Gen Food', '2250000004', 'Metairie');
-insert into Vendor values ('V024', 'Plaisance goods', '2250000005', 'Baton Rouge');
+insert into Vendor values ('V024', 'Plaisance Goods', '2250000005', 'Baton Rouge');
 insert into Vendor values ('V025', 'Trinhs', '2250000006', 'Baton Rouge');
 
 
@@ -129,7 +116,6 @@ insert into Rep values ('V005', 'R012', 'Sherry', '2257758237');
 insert into Rep values ('V007', 'R013', 'Nicole', '2258312356');
 insert into Rep values ('V008', 'R014', 'Sam', '2252969593');
 insert into Rep values ('V009', 'R015', 'John', '2257593768');
-insert into Rep values ('V009', 'R016', 'Cori', '2257685935');
 
 
 /* Insert into driver table */
@@ -142,5 +128,5 @@ insert into Driver values ('V006', 'D006', 'Debbie', '2257653425');
 insert into Driver values ('V007', 'D007', 'Sally', '2255662341');
 insert into Driver values ('V008', 'D008', 'Evan', '2257673422');
 insert into Driver values ('V009', 'D009', 'Victor', '2258765698');
-
+insert into Driver values ('V009', 'D010', 'Ian', '2258723385');
 
